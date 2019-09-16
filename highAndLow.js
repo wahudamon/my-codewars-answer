778e2ae598a8c6584d7db6575c851f5583f38983
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2019-09-16 22:34:43 +0700

    solve the high and low kata

---
+++ highAndLow.js
@@ -0,0 +1,17 @@
+//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+//Example
+//highAndLow("1 2 3 4 5"); return "5 1"
+//highAndLow("1 2 -3 4 5"); return "5 -3"
+//highAndLow("1 9 3 4 -5"); return "9 -5"
+
+
+//My Answer
+function highAndLow(numbers) {
+  var arrayOfNumber = numbers.split(" ")
+  var max = Math.max.apply(null, arrayOfNumber)
+  var min = Math.min.apply(null, arrayOfNumber)
+  console.log(max + " " + min)
+}
+
+highAndLow('1 2 3')
