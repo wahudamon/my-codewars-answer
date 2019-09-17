defb1b073ad983b2c6075d3873f64338b6eefbe5
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2019-09-17 08:52:39 +0700

    solved the sort only the odd number in array challenge

---
+++ sortArray.js
@@ -0,0 +1,21 @@
+//You have an array of numbers.
+//Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+//Zero isn't an odd number and you dong need to move it.
+//If you have an empty array, you need to return it.
+
+//Example
+//sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+//My Answer
+function sortArray(array) {
+  var helperArray = [];
+  array
+    .filter((v, i) => v % 2 && helperArray.push(i))
+    .sort((a, b) => a - b)
+    .forEach((v, i) => array[helperArray[i]] = v);
+  
+  return array;
+}
+
+sortArray([5, 3, 2, 8, 1, 4]);
